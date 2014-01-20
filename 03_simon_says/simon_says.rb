61b7f8fbedd7b30ad1c6d0c8c0ca0f06a9667e3b
alanjkg <alanjkg@gmail.com>
2014-01-20 18:12:55 -0500

    finished Simon

---
+++ 03_simon_says/simon_says.rb
@@ -0,0 +1,38 @@
+def echo(word)
+	return word
+end
+
+def shout(word)
+	return word.upcase
+end
+
+def repeat(word, num = 1)
+	if num ==1
+		return word +" "+word
+	else
+		return ("#{word} "  * num).strip
+	end
+end
+
+def start_of_word(word,number)
+    new_word = word[0,number]
+    return new_word
+end
+
+def first_word(word)
+new_word = word.split[0]
+end
+
+def titleize(words)
+        arrayW=[]
+        arrayLittleW = %w(and but if it or over the) #makes an array with words, space delimited, no quotes
+        words.split.each do |word|
+                if arrayLittleW.include? word
+                        arrayW << word
+                else
+                        arrayW <<word.capitalize
+                end
+        end
+        arrayW[0] = arrayW[0].capitalize
+        arrayW.join(' ')
+end
