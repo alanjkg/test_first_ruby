758a7e909535f0bfe5b20c430cb58de0d6d0a52d
alanjkg <alanjkg@gmail.com>
2014-01-26 22:11:20 -0500

    finished little miss piggy

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,8 +1,18 @@
 def translate (word)
 
-s = translate (word)
+	alphabet = ('a'..'z').to_a
+	vowel = ["a","e","i","o","u"]
+	consonant = alphabet - vowel
 
-new_word = word[0,number]
-if s==
-s.insert(-1, 'ay')
+	if vowel.include?(word[0])
+	word + "ay"
+
+	elsif consonant.include?(word[0]) && consonant.include?(word[1])
+	word[2..-1] + word[0..1] + "ay"
+
+	elsif consonant.include?(word[0])
+	word.reverse + "ay"
+else
+	word
+end
 end
